5e71a239a07f95b8a96647f8cd64029011e20d16
klayton-a-souza <klayton.al.souza@gmail.com>
2022-10-31 12:37:47 -0300

    Codigo 03: Código mais sucinto com Method references

--- Codigo/java8/src/java8/OrdenaStrings.java
+++ Codigo/java8/src/java8/OrdenaStrings.java
@@ -1,6 +1,7 @@
 package java8;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaStrings {
@@ -14,8 +15,18 @@ public class OrdenaStrings {
 		personagens.add("Sucrose");
 		personagens.add("Bennett");
 
-		personagens.sort((p1, p2) -> Integer.compare(p1.length(), p2.length()));
-		personagens.forEach(personagem -> System.out.println(personagem));
+//		personagens.sort((p1, p2) -> Integer.compare(p1.length(), p2.length()));		
+//		Codigo abaixo e similar ao codigo da linha 18
+//		personagens.sort(Comparator.comparing(p -> p.length()));
+//		personagens.sort(Comparator.comparing(String::length));
+
+//		Codigo 3.6: Mudando o critério de comparação
+		personagens.sort(String.CASE_INSENSITIVE_ORDER); // Ordem alfabetica
+
+//		personagens.forEach(personagem -> System.out.println(personagem));
+		
+//		Codigo 3.7: Tirando proveito de method reference no forEach
+		personagens.forEach(String::length);
 
 		System.out.println(personagens);
 
